62531cb61d7fa9708dca8e79325f011c577d4e3b
ts7n <65257768+ts7n@users.noreply.github.com>
2024-12-21 11:52:22 -0600

    Integrate into SBGBook

--- background.js
+++ background.js
@@ -1,9 +1,8 @@
 chrome.action.onClicked.addListener((tab) => {
-    if (tab.url.includes("sbgbook.xyz")) {
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ["contentScript.js"]
-      });
-    }
-  });
-  
+  if (tab.url.includes("sbgbook.xyz")) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ["contentScript.js"]
+    });
+  }
+});
